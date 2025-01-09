cb46a4a6e97423f7367edf7f1c05905e8f167695
Mateus Antônio <mateusantonio741@gmail.com>
2025-01-09 17:34:41 -0300

    'testeFactory'

--- database/factories/CategoriaFactory.php
+++ database/factories/CategoriaFactory.php
@@ -17,7 +17,8 @@ class CategoriaFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'nome'=> $this->faker->unique()->word,
+            'descricao'=>$this->faker->text,
         ];
     }
 }
